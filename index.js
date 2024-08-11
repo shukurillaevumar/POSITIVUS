ad9d2f418f3a9b9556099c628771d33f84cd025f
Muhammadumar <khilolamoon@gmail.com>
2024-08-11 11:57:59 +0500

    changes

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const slider = document.querySelector(".card-list");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+// const slider = document.querySelector(".card-list");
+// const prevButton = document.querySelector(".prev");
+// const nextButton = document.querySelector(".next");
 
-let currentIndex = 0;
+// let currentIndex = 0;
 
-function updateSliderPosition() {
-  const cardWidth = slider.querySelector(".card-item").clientWidth;
-  slider.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-}
+// function updateSliderPosition() {
+//   const cardWidth = slider.querySelector(".card-item").clientWidth;
+//   slider.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+// }
 
-prevButton.addEventListener("click", () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateSliderPosition();
-  }
-});
+// prevButton.addEventListener("click", () => {
+//   if (currentIndex > 0) {
+//     currentIndex--;
+//     updateSliderPosition();
+//   }
+// });
 
-nextButton.addEventListener("click", () => {
-  if (currentIndex < slider.children.length - 1) {
-    currentIndex++;
-    updateSliderPosition();
-  }
-});
+// nextButton.addEventListener("click", () => {
+//   if (currentIndex < slider.children.length - 1) {
+//     currentIndex++;
+//     updateSliderPosition();
+//   }
+// });
 
-window.addEventListener("resize", updateSliderPosition);
+// window.addEventListener("resize", updateSliderPosition);
